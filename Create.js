27b121e748a773135f0b9f3b1cf8e2f395936476
anindyo-choudhury-1995 <anindyochoudhury1995@gmail.com>
2020-05-27 15:49:56 +0530

    2 files modified

--- Create.js
+++ Create.js
@@ -8,6 +8,14 @@ class Create extends Component {
         <div className="Heading">
           My Service Requests - Solution Engineering
         </div>
+        <div className="ActionBar">
+          <button>
+            Submit Service Request
+          </button>
+          <button>
+            Cancel
+          </button>
+        </div>
         <div className="TwoColumns">
           <div className="Columns">
             <div className="ColumnItems">
@@ -99,6 +107,74 @@ class Create extends Component {
               <div className="ColumnItemLeft">Attachments</div>
               <div className="ColumnItemRight">
                 <button>Button</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="DescriptionBox">
+          <div>
+            Description*
+          </div>
+          <div className="DescriptionTextarea">
+            <textarea />
+          </div>
+        </div>
+        <div className="ServicesBox">
+          <div>
+            Services*
+          </div>
+          <div>
+            <b>Customer Workshop or Cloud Day -</b> One or many customer workshop or event focused on highlighting compelling and differentiated positioning of Oracle Solution and Capabilities.
+          </div>
+        </div>
+        <div>
+          <button>
+            Change Service Selection
+          </button>
+        </div>
+        <div className="QuestoionsBox">
+          <div>
+            Questions*
+          </div>
+          <div>
+            <div className="QuestionAnswers">
+              <div className="Question">
+              Any other information for this service request? (e.g. additional products, business context)?*
+              </div>
+              <div className="Answer">
+                <textarea />
+              </div>
+            </div>
+            <div className="QuestionAnswers">
+              <div className="Question">
+              Does this require on site resources?*
+              </div>
+              <div className="Answer">
+                <textarea />
+              </div>
+            </div>
+            <div className="QuestionAnswers">
+              <div className="Question">
+              What is the timeline identified to begin the service?*
+              </div>
+              <div className="Answer">
+                <textarea />
+              </div>
+            </div>
+            <div className="QuestionAnswers">
+              <div className="Question">
+              What needs to be highlighted for the customer?*
+              </div>
+              <div className="Answer">
+                <textarea />
+              </div>
+            </div>
+            <div className="QuestionAnswers">
+              <div className="Question">
+              Who are the key customer stakeholders and personas to target?*
+              </div>
+              <div className="Answer">
+                <textarea />
               </div>
             </div>
           </div>
